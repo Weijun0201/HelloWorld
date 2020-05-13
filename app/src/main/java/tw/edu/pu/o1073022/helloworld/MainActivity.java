58db29b41bb77b28e61c55a9d53b3657d9139dbf
Wei Jun <o1073022@gm.pu.edu.tw>
2020-05-13 10:36:33 +0800

    第二次 Commit(修改文字元件)

--- app/src/main/java/tw/edu/pu/o1073022/helloworld/MainActivity.java
+++ app/src/main/java/tw/edu/pu/o1073022/helloworld/MainActivity.java
@@ -2,6 +2,7 @@ package tw.edu.pu.o1073022.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txv =(TextView) findViewById(R.id.txv);
     }
 }
